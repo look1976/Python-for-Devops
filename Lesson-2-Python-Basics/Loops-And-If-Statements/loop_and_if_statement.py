e1c3ad55494778bfeb46ead23603e4150ca190e8
look1976 <65955437+look1976@users.noreply.github.com>
2021-05-05 18:51:29 +0200

    more basics

---
+++ Lesson-2-Python-Basics/Loops-And-If-Statements/loop_and_if_statement.py
@@ -0,0 +1,19 @@
+
+names = 'Shannon'
+
+for name in names:
+    if names == 'Shannon':
+    print('its Shannon')
+    break
+
+elif names == 'Brittany':
+    print('its Brittany)
+    break
+
+elif names == 'Mike':
+    print('its Mike')
+    break
+
+else:
+    print('No idea who')
+    break
