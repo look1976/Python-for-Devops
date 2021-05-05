9095ae84f4681235226e9a328da58260aad01ef6
look1976 <65955437+look1976@users.noreply.github.com>
2021-05-05 18:36:05 +0200

    more tests

---
+++ Lesson-2-Python-Basics/Types-And-Variables/other_types.py
@@ -0,0 +1,24 @@
+
+# Lists
+fruit = ['Apple', 'Banana', 'Strawberry']
+versions = ['v1', 'v2', 'v3']
+#print(fruit)
+#print(versions)
+
+# Range
+number = range(6)
+
+# Dictionary
+employees = {"Name": "John", "age": 30, "Location": "New Jersey"}
+#print(employees)
+
+# Bytes
+x = b"Hello"
+print(x)
+
+# FLoat
+x = float(20.5)
+
+# Boolean
+yes = True
+no = False
